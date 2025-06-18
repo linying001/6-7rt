22d008c8a6c308ef1ef33487dbe257b82bf22c22
linying001 <5b1g0004@stust.edu.tw>
2025-06-18 13:11:18 +0800

    01125

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>123456789陳小明</h1>
+      <h1>5b1g0004 林文楷</h1>
       <h2>我的第一個元件：Slider</h2>
       <div>
         <p>
